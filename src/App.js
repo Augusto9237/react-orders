589459866fb53f4156599da70fd663eea4ec60e0
Augusto9237 <augusto.souza8330@gmail.com>
2022-04-13 21:36:53 -0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -72,9 +72,6 @@ function Layout() {
         </nav>
       </Sidebar>
 
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
       <Outlet />
     </div>
   );
